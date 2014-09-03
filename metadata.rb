5a8b5759ee31b110007d8f58ed0cd70ee42fdfee
Antoine Rouyer <antoine.rouyer@numergy.com>
2014-09-03 14:52:10 +0200

    Licensed under Apache 2.0

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'ndenv'
 maintainer 'Antoine Rouyer'
 maintainer_email 'antoine.rouyer@numergy.com'
-license 'All rights reserved'
+license 'Apache 2.0'
 description 'Installs/Configures ndenv'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'

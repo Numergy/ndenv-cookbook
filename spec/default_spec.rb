e188417a9a63b4120872462eaa0a8040ed0f12d4
Pierre RAMBAUD <pierre.rambaud@numergy.com>
2014-09-04 12:26:40 +0200

    Add more tests for chefspec

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -15,4 +15,67 @@ describe 'ndenv::default' do
   it 'does install packages' do
     expect(subject).to install_package('curl')
   end
+
+  it 'should create group' do
+    expect(subject).to create_group('ndenv').with(
+      members: []
+    )
+  end
+
+  it 'should create user' do
+    expect(subject).to create_user('ndenv').with(
+      shell: '/bin/bash',
+      group: 'ndenv',
+      supports: { manage_home: true },
+      home: '/home/ndenv'
+    )
+  end
+
+  it 'should prepare directory' do
+    expect(subject).to create_directory('/opt/ndenv').with(
+      user: 'ndenv',
+      group: 'ndenv',
+      mode: '0755',
+      recursive: true
+    )
+  end
+
+  it 'should clone ndenv repository' do
+    expect(subject).to sync_git('/opt/ndenv').with(
+      user: 'ndenv',
+      group: 'ndenv',
+      repository: 'https://github.com/riywo/ndenv.git',
+      reference: 'master'
+    )
+  end
+
+  it 'should prepare plugins directory' do
+    expect(subject).to create_directory('/opt/ndenv/plugins').with(
+      user: 'ndenv',
+      group: 'ndenv',
+      mode: '0755',
+      recursive: true
+    )
+  end
+
+  it 'should clone node-build repository' do
+    expect(subject).to sync_git('/opt/ndenv/plugins/node-build').with(
+      user: 'ndenv',
+      group: 'ndenv',
+      repository: 'https://github.com/riywo/node-build.git',
+      reference: 'master'
+    )
+  end
+
+  it 'should create profile directory' do
+    expect(subject).to_not create_directory('/etc/profile.d')
+  end
+
+  it 'should copy ndenv profile file' do
+    expect(subject).to create_template('/etc/profile.d/ndenv.sh').with(
+      owner: 'ndenv',
+      group: 'ndenv',
+      mode: '0644'
+    )
+  end
 end

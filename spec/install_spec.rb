14bac22bf32897c10040fd5c491573af01dd27bb
Sliim <sliim@mailoo.org>
2014-09-11 19:04:54 +0200

    ChefSpec improvement for ndenv::install recipe

--- spec/install_spec.rb
+++ spec/install_spec.rb
@@ -2,28 +2,31 @@
 
 require_relative 'spec_helper'
 
-describe 'ndenv::install with default attributes' do
+describe 'ndenv::install' do
+  describe 'install with default attributes' do
 
-  let(:chef_run) { ChefSpec::Runner.new.converge 'ndenv::install' }
+    let(:chef_run) { ChefSpec::Runner.new.converge described_recipe }
 
-  it 'does install node version 0.10.26' do
-    expect(chef_run).to install_ndenv_node('0.10.26').with(global: true)
-  end
-end
-
-describe 'ndenv::install with overriden attributes' do
-  let(:chef_run) do
-    ChefSpec::Runner.new do |node|
-      node.set['ndenv']['installs'] = ['0.10.20', '0.10.26']
-      node.set['ndenv']['global'] = '0.10.26'
-    end.converge 'ndenv::install'
+    it 'does install node version 0.10.26' do
+      expect(chef_run).to install_ndenv_node('0.10.26').with(global: true)
+    end
   end
 
-  it 'does install node version 0.10.20' do
-    expect(chef_run).to install_ndenv_node('0.10.20').with(global: false)
+  describe 'install with overriden attributes' do
+    let(:chef_run) do
+      ChefSpec::Runner.new do |node|
+        node.set['ndenv']['installs'] = ['0.10.20', '0.10.26']
+        node.set['ndenv']['global'] = '0.10.26'
+      end.converge described_recipe
+    end
+
+    it 'does install node version 0.10.20' do
+      expect(chef_run).to install_ndenv_node('0.10.20').with(global: false)
+    end
+
+    it 'does install node version 0.10.26' do
+      expect(chef_run).to install_ndenv_node('0.10.26').with(global: true)
+    end
   end
 
-  it 'does install node version 0.10.26' do
-    expect(chef_run).to install_ndenv_node('0.10.26').with(global: true)
-  end
 end

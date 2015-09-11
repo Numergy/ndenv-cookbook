2ef3a1e57c2e6c16ddfa2b2ded4add1de865d9fe
Pierre RAMBAUD <pierre.rambaud@numergy.com>
2015-09-11 12:03:08 +0200

    Fix tests and problem with ndenv_npm resource

--- providers/npm.rb
+++ providers/npm.rb
@@ -8,52 +8,51 @@ end
 use_inline_resources
 
 action :install do
-  name = new_resource.package_name
-  version  = new_resource.version
+  name = @new_resource.package_name
+  version  = @new_resource.version
+  node_version = @new_resource.node_version
   npm_args = 'install -g '
 
-  if new_resource.source
+  if @new_resource.source
     npm_args << @new_resource.source
   else
     npm_args << name
     npm_args << "@#{version}" if version
   end
 
-  out = npm_command("-g -j ls #{name}",  @new_resource.node_version)
+  out = npm_command("-g -j ls #{name}", node_version)
+  present_version = ::JSON.parse(out.stdout.strip)['dependencies'][name]['version'] rescue nil
 
-  if out.exitstatus == 0
+  unless present_version.nil?
     # check for version equality only if version is specified
     if version
-      present_version = ::JSON.parse(out.stdout.strip)['dependencies'][name]['version']
-      if version != present_version
-        converge_by "A different version (#{present_version}) of npm package is present. Will install version #{version}" do
-          npm_command!(npm_args, @new_resource.node_version)
-          ndenv_command!('rehash')
-        end
-      end
+      converge_by "A different version (#{present_version}) of npm package is present. Will install version #{version}" do
+        npm_command!(npm_args, node_version)
+        ndenv_command!('rehash')
+      end if version != present_version
     end
   else
     converge_by "npm package #{name} is not present, installing it" do
-      npm_command!(npm_args, @new_resource.node_version)
+      npm_command!(npm_args, node_version)
       ndenv_command!('rehash')
     end
   end
 end
 
 action :upgrade do
-  name = new_resource.package_name
+  name = @new_resource.package_name
   converge_by("Upgrade NPM package `#{name}` for node[#{new_resource.node_version}]..") do
-    npm_command!("update -g #{name}", new_resource.node_version)
+    npm_command!("update -g #{name}", @new_resource.node_version)
     ndenv_command!('rehash')
   end
 end
 
 action :remove do
-  name = new_resource.package_name
-  out = npm_command("-g ls #{name}",  new_resource.node_version)
+  name = @new_resource.package_name
+  out = npm_command("-g ls #{name}", @new_resource.node_version)
   unless out.exitstatus == 0
     converge_by("Remove NPM package `#{name}` for node[#{new_resource.node_version}]..") do
-      npm_command!("remove -g #{name}", new_resource.node_version)
+      npm_command!("remove -g #{name}", @new_resource.node_version)
       ndenv_command!('rehash')
     end
   end
